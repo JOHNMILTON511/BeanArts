86e9b9b371329303dfb439a4543182a7ae6997b9
John Milton <miltonjohn511@gmail.com>
2025-11-06 12:16:06 +0530

    home page addded

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideAnimations(),
+        importProvidersFrom(CarouselModule),
+
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CarouselModule  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  heroCarouselOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      900: {
+        items: 1
+      }
+    },
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+  };
 
+  // Slides array (used in @for)
+  slides = [
+    {
+      id: 1,
+      imgSrc: 'Slider/1.jpg',
+      title: 'Discover Creativity',
+      subtitle: 'Where art meets imagination'
+    },
+    {
+      id: 2,
+      imgSrc: 'Slider/2.jpg',
+      title: 'Modern Expression',
+      subtitle: 'Experience the fusion of art and technology'
+    },
+    {
+      id: 3,
+      imgSrc: 'Slider/3.jpg',
+      title: 'Timeless Beauty',
+      subtitle: 'Capturing emotions through every stroke'
+    }
+  ];
+
+  
 }
