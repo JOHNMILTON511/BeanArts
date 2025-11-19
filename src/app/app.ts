a1149c8d38dd545c160d6ae9c55a73476dd6529b
John Milton <miltonjohn511@gmail.com>
2025-11-20 00:16:00 +0530

    optimzed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { afterNextRender, Component, signal } from '@angular/core';
 import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
+import Aos from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,16 @@ import { Footer } from './shared/footer/footer';
 })
 export class App {
   protected readonly title = signal('BeanArts');
+
+  constructor() {
+    // afterNextRender is a new hook in Angular 17+ that runs ONLY in the browser
+    // ensuring this code never runs on the server (SSR)
+    afterNextRender(() => {
+      Aos.init({
+        once: true,
+        duration: 800,
+        easing: 'ease-in-out',
+      });
+    });
+  }
 }
